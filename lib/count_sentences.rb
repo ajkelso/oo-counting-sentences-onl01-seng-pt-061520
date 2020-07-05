71c4d14113d870ac4ee1bced4358e7dc841de8ad
ajkelso <ajkelso@gmail.com>
2020-07-05 09:44:14 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+  
   end
 
   def question?
+
+    self.end_with?("?")
 
   end
 
   def exclamation?
 
+    self.end_with?("!")
+
   end
 
   def count_sentences
 
+    total = self.split(/[.!?]/)
+    total.each do |string|
+      total.delete(string) if string = ""
+    end
+    total.count
   end
 end
